80f313e4cb28ea634948197ff568c087b1cc2a25
DuDungSil <121501087+DuDungSil@users.noreply.github.com>
2025-05-16 19:26:52 +0900

    fix: BlockProperty
    블록 컨버터 수정

--- modules/note-service/src/main/java/org/example/note/domain/entity/Block.java
+++ modules/note-service/src/main/java/org/example/note/domain/entity/Block.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 
 import org.example.note.domain.enums.BlockType;
 import org.example.note.domain.property.BlockProperties;
+import org.example.note.domain.property.BlockPropertiesConverter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.type.SqlTypes;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -42,7 +43,8 @@ public class Block {
     @Enumerated(EnumType.STRING)
     private BlockType type;
 
-    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    @Convert(converter = BlockPropertiesConverter.class)
     private BlockProperties properties;
 
     private Integer position;
